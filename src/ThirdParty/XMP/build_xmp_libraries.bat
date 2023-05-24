6cde9af47934ac125ceb6242ba522a4da9dfb5c8
Bryan Pakulski <BryanPakulski@outlook.com>
2023-05-24 11:25:52 +1000

    base windows xmp build

--- src/ThirdParty/XMP/build_xmp_libraries.bat
+++ src/ThirdParty/XMP/build_xmp_libraries.bat
@@ -1,1 +1,61 @@
-:: TODO: Build windows version of this script...
+@echo off
+
+if not exist "src\" (
+    echo please run this script from the root directory
+    exit 1
+)
+
+mkdir lib
+set "lib_output=%CD%\lib"
+
+cd src\ThirdParty\XMP
+
+if not exist "XMP-Toolkit-SDK\" (
+    :: Setup base library
+    git clone https://github.com/adobe/XMP-Toolkit-SDK
+
+    :: Setup zlib
+    powershell -Command "(New-Object System.Net.WebClient).DownloadFile('https://www.zlib.net/zlib-1.2.13.tar.gz', 'zlib-1.2.13.tar.gz')"
+    :: Extract and copy .c and .h files
+    tar -xzvf zlib-1.2.13.tar.gz
+    move zlib-1.2.13\*.c XMP-Toolkit-SDK\third-party\zlib
+    move zlib-1.2.13\*.h XMP-Toolkit-SDK\third-party\zlib
+    del zlib-1.2.13.tar.gz
+    rmdir zlib-1.2.13 /s /q
+
+    :: Set up libexpat
+    git clone https://github.com/libexpat/libexpat
+    move libexpat\expat\lib XMP-Toolkit-SDK\third-party\expat\
+    rmdir libexpat /s /q
+)
+
+cd XMP-Toolkit-SDK\build
+
+
+Echo Building SDK
+echo "Generating XMPSDKToolkit Dynamic x64"
+set NEXT_LABEL=ok
+set VS_VERSION=2019
+set BUILD_TYPE=Dynamic
+set BITS=64
+IF "%GENERATE_ALL%"=="On" (
+	set NEXT_LABEL=ok
+)
+
+call cmake_all.bat %BITS% %VS_VERSION% WarningAsError %BUILD_TYPE%
+if errorlevel 1 goto error
+goto %NEXT_LABEL%
+
+:error
+echo CMake Build Failed.
+pause
+exit /B 1
+
+:ok
+echo CMake Build Success.
+pause
+exit /B 0
+
+cd ..\..\..
+
+:: Copy static libs
